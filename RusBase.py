d5d74d6f7115cd4642c65a8e4eb595c93cf401d6
Ruslan Shakirov <rshak001@plattsburgh.edu>
2017-10-07 21:04:04 -0400

    Added buttons in tab2.
    Tab2 was renamed to 'End-user Software'. Added buttons to tab, and adjusted their sizes accordingly.

--- RusBase.py
+++ RusBase.py
@@ -19,7 +19,7 @@ class Application(Frame):
         self.master = master
         self.logo()
         self.menu_bar()
-        self.create_widgets()
+        self.tab_buttons()
 
     def logo(self):
         #Set frame size and style for Logo 
@@ -55,23 +55,18 @@ class Application(Frame):
         #Add 'edit' to the menu.
         menu.add_cascade(label='Edit',menu=edit)
 
-    def create_widgets(self):
+    def tab_buttons(self):
          """
          """
          tab1 = Frame(note, width=800, height=500, bg="powder blue", relief=SUNKEN)
          tab2 = Frame(note, width=800, height=500, bg="powder blue", relief=SUNKEN)
          tab3 = Frame(note, width=800, height=500, bg="powder blue", relief=SUNKEN)
          note.add(tab1, text = "OS Tools")
-         note.add(tab2, text = "Audit 2")
+         note.add(tab2, text = "End-User Software")
          note.add(tab3, text = "Remove")
          note.pack()
 
-         """T = Text(tab1, height=1, width=5, bg="powder blue",fg="black",font=('arial',13,'bold'))
-         T.pack()
-         T.insert(END, "HDD")
-         T.place(x=0,y=10)"""
-         
-         #Placing first six buttons in tab1.
+         #Place buttons in first column of tab1.
          btn1=Button(tab1,padx=41,bd=2,fg="black",font=('arial',10,'bold'),text="Crystal Disk",command=self.btn1Click)
          btn1.place(x=8,y=10)
 
@@ -107,9 +102,8 @@ class Application(Frame):
 
          btn12=Button(tab1,padx=62,bd=2,fg="black",font=('arial',10,'bold'),text="Revo",command='')
          btn12.place(x=8,y=450)
-
-
          
+         #Place buttons in second column of tab1.
          
          btn13=Button(tab1,padx=41,bd=2,fg="black",font=('arial',10,'bold'),text="WinDirStat",command=self.btn1Click)
          btn13.place(x=230,y=10)
@@ -147,13 +141,89 @@ class Application(Frame):
          btn24=Button(tab1,padx=59,bd=2,fg="black",font=('arial',10,'bold'),text="Revo",command='')
          btn24.place(x=230,y=450)
 
+        
+         #Place buttons in first column of tab2.
+         btn25=Button(tab2,padx=60,bd=2,fg="black",font=('arial',10,'bold'),text="Ninite",command=self.btn1Click)
+         btn25.place(x=8,y=10)
+
+         btn26=Button(tab2,padx=28,bd=2,fg="black",font=('arial',10,'bold'),text="Google Chrome",command='')
+         btn26.place(x=8,y=50)
+
+         btn27=Button(tab2,padx=8,bd=2,fg="black",font=('arial',10,'bold'),text="Chrome Cleanup Tool",command='')
+         btn27.place(x=8, y=90)
+
+         btn27=Button(tab2,padx=31,bd=2,fg="black",font=('arial',10,'bold'),text="Mozilla Firefox",command='')
+         btn27.place(x=8, y=130)
+
+         btn28=Button(tab2,padx=58,bd=2,fg="black",font=('arial',10,'bold'),text="Opera",command='')
+         btn28.place(x=8, y=170)
+
+         btn29=Button(tab2,padx=63,bd=2,fg="black",font=('arial',10,'bold'),text="7-Zip",command='')
+         btn29.place(x=8,y=210)
+
+         btn30=Button(tab2,padx=53,bd=2,fg="black",font=('arial',10,'bold'),text="WinRaR",command='')
+         btn30.place(x=8,y=250)
+         
+         btn31=Button(tab2,padx=59,bd=2,fg="black",font=('arial',10,'bold'),text="Skype",command='')
+         btn31.place(x=8,y=290)
+
+         btn32=Button(tab2,padx=40,bd=2,fg="black",font=('arial',10,'bold'),text="Thunderbird",command='')
+         btn32.place(x=8, y=330)
+
+         btn33=Button(tab2,padx=52,bd=2,fg="black",font=('arial',10,'bold'),text="Dropbox",command='')
+         btn33.place(x=8, y=370)
+
+         btn34=Button(tab2,padx=38,bd=2,fg="black",font=('arial',10,'bold'),text="Google Drive",command='')
+         btn34.place(x=8, y=410)
+
+         btn35=Button(tab2,padx=50,bd=2,fg="black",font=('arial',10,'bold'),text="OneDrive",command='')
+         btn35.place(x=8,y=450)
+         
+         #Place buttons in second column of tab1.
+         
+         btn36=Button(tab2,padx=53 ,bd=2,fg="black",font=('arial',10,'bold'),text="ITunes",command=self.btn1Click)
+         btn36.place(x=230,y=10)
+
+         btn37=Button(tab2,padx=61,bd=2,fg="black",font=('arial',10,'bold'),text="VLC",command='')
+         btn37.place(x=230,y=50)
+
+         btn38=Button(tab2,padx=57,bd=2,fg="black",font=('arial',10,'bold'),text="AIMP",command='')
+         btn38.place(x=230, y=90)
+
+         btn39=Button(tab2,padx=48,bd=2,fg="black",font=('arial',10,'bold'),text="Winamp",command='')
+         btn39.place(x=230, y=130)
+
+         btn40=Button(tab2,padx=31,bd=2,fg="black",font=('arial',10,'bold'),text="K-Lite Codecs",command='')
+         btn40.place(x=230, y=170)
+
+         btn41=Button(tab2,padx=59,bd=2,fg="black",font=('arial',10,'bold'),text="GOM",command='')
+         btn41.place(x=230,y=210)
+
+         btn42=Button(tab2,padx=52,bd=2,fg="black",font=('arial',10,'bold'),text="Spotify",command='')
+         btn42.place(x=230,y=250)
+         
+         btn43=Button(tab2,padx=54,bd=2,fg="black",font=('arial',10,'bold'),text="Java 8",command='')
+         btn43.place(x=230,y=290)
+
+         btn44=Button(tab2,padx=39,bd=2,fg="black",font=('arial',10,'bold'),text="NotePad++",command='')
+         btn44.place(x=230, y=330)
+
+         btn45=Button(tab2,padx=48,bd=2,fg="black",font=('arial',10,'bold'),text="WinSCP",command='')
+         btn45.place(x=230, y=370)
+
+         btn46=Button(tab2,padx=58,bd=2,fg="black",font=('arial',10,'bold'),text="Putty",command='')
+         btn46.place(x=230, y=410)
+
+         btn47=Button(tab2,padx=49,bd=2,fg="black",font=('arial',10,'bold'),text="FileZilla",command='')
+         btn47.place(x=230,y=450)
+
     def btn1Click(btn):
         try:
             from urllib.request import urlretrieve
         except ImportError:
             from urllib import urlretrieve
-        urlretrieve('https://drive.google.com/uc?export=download&id=0B06Qp2heoHZkdkQyN0gtaUxNNkU', 'rufus-2.15.exe')
-        file="rufus-2.15.exe"
+        urlretrieve('https://drive.google.com/uc?export=download&id=0B06Qp2heoHZkMWJlSDRlYTlmczA', 'CrystalDisk.exe')
+        file="CrystalDisk.exe"
         btn=subprocess.call([file], shell=True)
         
     def btn2Click(btn):
